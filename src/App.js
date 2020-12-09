21e66b1f23e9debc15e8cc93a2bf5dfb49dff13f
Fauz Ali <fauz_sp@hotmail.com>
2020-12-10 02:04:01 +0500

    changes in app routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import "./App.css";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/homepage-component/homepage.component.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
-      </Switch>
-    </div>
-  );
-}
+import "./App.css";
+
+import HomePage from "./pages/homepage-component/homepage.component.jsx";
 
 const HatsPage = () => {
   return (
     <div>
-      <h1>Hats Page</h1>
+      <h1>HATS PAGE </h1>
     </div>
   );
 };
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
+}
 
 export default App;
