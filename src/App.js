b8aba5d13ca0245165e0e911954a1ff0ace15a60
Fauz Ali <fauz_sp@hotmail.com>
2020-12-11 02:41:52 +0500

    shop page created and linked in components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
 import "./App.css";
-
 import HomePage from "./pages/homepage-component/homepage.component.jsx";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE </h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
