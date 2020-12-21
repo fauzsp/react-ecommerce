ab43b4c3086439cf7433f9453dfca5ca03ceb807
Fauz Ali <fauz_sp@hotmail.com>
2020-12-22 00:58:08 +0500

    completed sign in and sign up functionality with firebase authentication

--- src/App.js
+++ src/App.js
@@ -3,18 +3,73 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/commons/header/header.component.jsx";
 import HomePage from "./pages/homepage-component/homepage.component.jsx";
+import SignInAndSignUp from "./pages/signin-signup/signin-signup.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import { auth, checkUserAuth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+      showHeader: false,
+    };
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const userRef = await checkUserAuth(user);
+        console.log(userRef, "hello");
+        userRef.onSnapshot((snapshot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapshot.id,
+                ...snapshot.data(),
+              },
+              showHeader: true,
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      } else {
+        this.setState({
+          showHeader: true,
+        });
+        console.log(this.state, "user does not exist");
+      }
+    });
+  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.showHeader !== this.state.showHeader) {
+  //     console.log("status changed");
+  //   } else {
+  //     console.log("status not changed");
+  //   }
+  // }
+  render() {
+    const handleClick = (e) => {
+      auth.signOut();
+      window.location.reload();
+    };
+    return (
+      <div>
+        <Header
+          currentUser={this.state.currentUser}
+          showHeader={this.state.showHeader}
+          handleClick={handleClick}
+        />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/commons/header/header.component.jsx
+++ src/components/commons/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as HeaderLogo } from "../../../assets/images/header-logo.svg";
+import { auth } from "../../../firebase/firebase.utils.js";
 import "./header.component.scss";
 
-const Header = () => {
+const Header = ({ currentUser, showHeader, handleClick }) => {
   return (
     <div className="header">
       <Link className="logo-container" to={"/"}>
@@ -16,6 +17,28 @@ const Header = () => {
         <Link to={"/cart"} className="option">
           CART
         </Link>
+        {showHeader ? (
+          <div>
+            {currentUser ? (
+              <div onClick={handleClick} className="option">
+                SIGN OUT
+              </div>
+            ) : (
+              <Link to={"/signin"} className="option">
+                SIGN IN
+              </Link>
+            )}
+          </div>
+        ) : null}
+        {/* {currentUser ? (
+          <div onClick={() => auth.signOut()} className="option">
+            SIGN OUT
+          </div>
+        ) : (
+          <Link to={"/signin"} className="option">
+            SIGN IN
+          </Link>
+        )} */}
       </div>
     </div>
   );
