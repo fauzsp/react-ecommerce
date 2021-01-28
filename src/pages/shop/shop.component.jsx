9bd00b5fe08ae356ac5284fc839deeb55e76b682
Fauz Ali <fauz_sp@hotmail.com>
2021-01-29 00:08:17 +0500

    redux persist library added and new components created

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,20 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview";
+import { Route } from "react-router-dom";
+// import { connect } from "react-redux";
+// import { createStructuredSelector } from "reselect";
+// import { selectCollections } from "../../redux/shop/shop.selector";
+import CategoryPage from "../category/category-page.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import "./shop.component.scss";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div>
-        {collections.map(({ id, ...otherCollection }) => (
-          <CollectionPreview key={id} {...otherCollection} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => {
+  console.log(match.path, "match");
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
