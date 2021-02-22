bd2dee4abec2d8f6aaaacaec26673d19bd2fdcca
Fauz Ali <fauz_sp@hotmail.com>
2021-02-23 01:15:00 +0500

    styled component library added and code pushed

---
+++ src/components/utilities/custom-button/custom-buttons.styles.jsx
@@ -0,0 +1,53 @@
+import styled, {css} from "styled-components";
+
+
+const buttonStyles = css`
+  background-color: black;
+  color: white;
+  border:none;
+  &:hover {
+      background-color: white;
+      color: black;
+      border:1px solid black;
+  }
+`
+const getButtonStyles = props => {
+    if(props.isGoogleSignIn) {
+        return googleSignInButton;
+    }
+    return props.inverted ? invertedButtonStyles : buttonStyles;
+}
+const invertedButtonStyles = css`
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+    &:hover {
+        background-color: black;
+        color: white;
+        border: none;
+    }
+`
+const googleSignInButton = css`
+    background-color: #4285f4;
+    &:hover {
+      background-color: #357ae8;
+      color:#ccc;
+      border: none;
+    }
+`
+
+export const CustomButtonContainer = styled.button`
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  text-align: center;
+  font-size: 15px;
+  text-transform: uppercase;
+  font-family: "Open Sans Condensed";
+  font-weight: bolder;
+  border: none;
+  cursor: pointer;
+  ${buttonStyles}
+`;
